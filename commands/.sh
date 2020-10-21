8434acf0a9d2409fb2381f38833bde2942c3e35a
leonore <29406849+leonore@users.noreply.github.com>
2020-10-21 16:06:06 +0100

    pipe stdout and f/r

--- commands/.sh
+++ commands/.sh
@@ -11,3 +11,9 @@ cp -R origin/ dest/
 grep foo -m 2         # number of hits
 grep foo -m 1 -A 5    # number of lines to show after the hit
 grep foo -m 1 -B 5    # number of lines to show before the hit
+
+# pipe stdout to a file
+command-generating-stdout 2>&1 | tee outfile
+
+# basic find and replace in a file
+sed -i 's/original/new/g' file.txt
