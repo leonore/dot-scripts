fd696dbd0d60ae4e7d42c8dff5660b8c945d6acd
leonore <29406849+leonore@users.noreply.github.com>
2021-11-17 15:17:06 +0000

    add recursive sed and convenient kubectl delete

--- commands/.sh
+++ commands/.sh
@@ -17,3 +17,9 @@ command-generating-stdout 2>&1 | tee outfile
 
 # basic find and replace in a file
 sed -i 's/original/new/g' file.txt
+
+# recursive find and replace in a directory
+grep -rl pattern . | xargs sed -i 's/old/new/g'
+
+# get k8s resources and act on them
+kubectl get pods | awk '/pattern/{print $1}' | xargs kubectl delete pod
